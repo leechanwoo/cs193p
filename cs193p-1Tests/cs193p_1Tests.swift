02a009c26ed829b62dfbdbaf6705f7b8dd8be326
Lee Chanwoo <chan@resonance2heart.com>
2020-06-28 09:33:56 +0900

    Initial Commit

---
+++ cs193p-1Tests/cs193p_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  cs193p_1Tests.swift
+//  cs193p-1Tests
+//
+//  Created by chanwoo on 2020/06/28.
+//  Copyright © 2020 chanwoo. All rights reserved.
+//
+
+import XCTest
+@testable import cs193p_1
+
+class cs193p_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
